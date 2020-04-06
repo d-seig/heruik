48065ba3ac3d5361f6d79f92aec8298f80eebbb7
d-seig <62769600+d-seig@users.noreply.github.com>
2020-04-06 21:54:22 +0300

    Update бот.js
    Some fixes..

--- бот.js
+++ бот.js
@@ -5,7 +5,7 @@ const client = new Discord.Client();
 var prefix = '!'
  
  client.on('message', message => {
-    if(message.author === client.user) return;
+    //if(message.author === client.user) return;
     if(message.content.startsWith(prefix + 'help')) {
         message.channel.sendMessage('Привет я БОТ!');
      }
